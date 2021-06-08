fa1b835397073248fd46d9c5e8721143cb6480e9
Seiryu-alt <seiryusain@gmail.com>
2021-06-08 13:16:30 +0000

    [add] ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :users
-  namespace :public do
-    get 'tags/show'
+  devise_for :users, only: [:sessions, :registrations], controllers: {
+    :sessions => 'public/users/sessions',
+    :registrations => 'public/users/registrations'
+  }
+
+  scope module: 'public' do
+    root :to    => 'homes#top'
+    get 'about' => 'homes#about'
+    resources :users, only: [:show] do
+      resource :relationship, only: [:create, :destroy] do
+        get 'following'
+        get 'follower'
+      end
+      resources :likes, only: [:index]
+    end
+
+    resources :post_images, only: [:show, :edit, :update, :destroy, :new, :create] do
+      collection do
+        get 'search'
+      end
+      resources :post_image_comments, only: [:create, :destroy]
+      resource :like, only: [:create, :destroy]
+    end
+    
+    resources :tags, only: [:show]
   end
-  namespace :public do
-    get 'likes/index'
-  end
-  namespace :public do
-    get 'post_images/show'
-    get 'post_images/edit'
-    get 'post_images/new'
-    get 'post_images/search'
-  end
-  namespace :public do
-    get 'relationships/following'
-    get 'relationships/follower'
-  end
-  namespace :public do
-    get 'users/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
