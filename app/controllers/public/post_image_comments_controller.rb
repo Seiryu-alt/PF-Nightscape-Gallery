6427766d5de16450502a706785f683cf5adcd157
Seiryu-alt <seiryusain@gmail.com>
2021-06-10 07:45:29 +0000

    [feature] 投稿コメント関連処理

--- app/controllers/public/post_image_comments_controller.rb
+++ app/controllers/public/post_image_comments_controller.rb
@@ -2,8 +2,21 @@ class Public::PostImageCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    post_image = PostImage.find(params[:post_image_id])
+    @post_iamge_comment = post_image.post_image_comments.new(post_image_comment_params)
+    @post_iamge_comment.user_id = current_user.id
+    @post_iamge_comment.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    post_iamge_comment = PostImageComment.find(params[:id])
+    post_iamge_comment.destroy
+  end
+
+  private
+
+  def post_image_comment_params
+    params.permit(:post_iamge_comment).require(:comment)
   end
 end
