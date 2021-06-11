b599b4bf701c61c48cb57f5f1784fdcbec992413
Seiryu-alt <seiryusain@gmail.com>
2021-06-11 06:03:19 +0000

    [feature] トップページ処理

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
+    @most_liked_post_images = @PostImage.includes(:likes)
+                                        .sort_by { |post_image| post_image.likes.size }
+                                        .reverse.limit(5)
+    @post_images = PostImage.all.order(updated_at: :desc).page(params[:page]).per(10)
   end
 
   def about
