f310b12089b49c2dd09c78b37c6ed8daeaa490cf
Seiryu-alt <seiryusain@gmail.com>
2021-06-11 02:57:58 +0000

    [add] マイページ処理追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,4 +2,10 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def mypage
+    following_users_id = current_user.following_users.pluck(:id)
+
+    @post_images = PostImage.where(user_id: following_users_id).order(updated_at: :desc)
+  end
 end
