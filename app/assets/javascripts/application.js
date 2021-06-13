8379d029c77b0e09c234c5b95506312a37023799
Seiryu-alt <seiryusain@gmail.com>
2021-06-13 06:24:35 +0000

    [add] デプロイ用設定 追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,13 +19,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-$(document).on('turbolinks:load', function() {
-  $(function () {
-    $('.jscroll').jscroll({
-      contentSelector: '.jscroll li',
-      nextSelector: 'span.next:last a',
-      loadingHtml: '<div class="text-center"><div class="spinner-grow text-primary" role="status"></div></div>'
-    });
-  });
-});
