182b531a41ca6a8e339e11c8dcc02311ba0f9be9
Seiryu-alt <seiryusain@gmail.com>
2021-06-15 01:34:35 +0000

    [feature] フォローボタン

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,13 +2,13 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
   end
 
   def following
