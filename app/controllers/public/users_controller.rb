079742d4bb711b9ca44c774f9af8c33cdead762f
Seiryu-alt <seiryusain@gmail.com>
2021-06-15 03:36:56 +0000

    [feature] ユーザープロフィールページ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,7 @@
 class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @post_images = @user.post_images.order(updated_at: :desc).page(params[:page]).per(3)
   end
 
   def mypage
