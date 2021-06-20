71fb9039788f42ae2c401e5501281e58245d184c
Seiryu-alt <seiryusain@gmail.com>
2021-06-20 13:00:12 +0000

    [feature] トップページの画像サイズを縮小

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,7 +4,7 @@ class Public::HomesController < ApplicationController
                                       .sort_by { |post_image| post_image.likes.size }
                                       .last(5)
     gon.bg_img_urls = most_liked_post_images.map do |post_image|
-      { src: Refile.attachment_url(post_image, :image, :limit, 1500, 1500, format: 'jpg') }
+      { src: Refile.attachment_url(post_image, :image, :limit, 1000, 1000, format: 'jpg') }
     end
     gon.post_image_urls = most_liked_post_images.map { |post_image| post_image_path(post_image) }
     @post_images = PostImage.all.order(updated_at: :desc).page(params[:page]).per(3)
